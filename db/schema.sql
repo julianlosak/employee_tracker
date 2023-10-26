95c49ac157016ed36f010865e87780540ef49dee
julianlosak <julianlosak@gmail.com>
2023-10-26 13:39:27 -0500

    adding installed inquireer

--- db/schema.sql
+++ db/schema.sql
@@ -1,6 +1,8 @@
 DROP DATABASE IF EXISTS employee_db;
 CREATE DATABASE employee_db;
 
+USE employee_db;
+
 CREATE TABLE department (
     id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     name VARCHAR(30) NOT NULL
@@ -9,15 +11,17 @@ CREATE TABLE department (
 CREATE TABLE role (
     id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     title VARCHAR(30) NOT NULL,
-    salary DECIMAL(6) NOT NULL,
-    department_id INT REFERENCES department(id) ON DELETE SET NULL
+    salary DECIMAL(10, 2) NOT NULL,
+    department_id INT,
+    FOREIGN KEY (department_id) REFERENCES department(id)
 );
-
 
 CREATE TABLE employee (
     id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     first_name VARCHAR(30) NOT NULL,
     last_name VARCHAR(30) NOT NULL,
-    role_id INT REFERENCES role(id) ON DELETE SET NULL,
-    manager_id INT REFERENCES employee(id)
+    role_id INT,
+    manager_id INT,
+    FOREIGN KEY (role_id) REFERENCES role(id),
+    FOREIGN KEY (manager_id) REFERENCES employee(id)
 );
--- db/seeds.sql
+++ db/seeds.sql
@@ -1,25 +1,25 @@
 INSERT INTO department (name)
 VALUES ("Marketing"),
-        ("Human Recouces"),
-        ( "Sales"),
-        ( "Customer Service");
+        ("Human Resources"),
+        ("Sales"),
+        ("Customer Service");
 
 INSERT INTO role (title, salary, department_id)
 VALUES ("Project Manager", 75000, 1),
-        ("Project Cordinator", 70000, 1),
+        ("Project Coordinator", 70000, 1),
         ("HR Manager", 60000, 2),
-        ( "Receptionist", 50000,2),
-        ( "Sales Manager", 120000, 3),
-        ( "Sales Person", 100000, 3),
-        ( "Phone Operator", 50000, 4),
-        ( "IT Director", 150000, 4);
+        ("Receptionist", 50000, 2),
+        ("Sales Manager", 120000, 3),
+        ("Sales Person", 100000, 3),
+        ("Phone Operator", 50000, 4),
+        ("IT Director", 150000, 4);
 
 INSERT INTO employee (first_name, last_name, role_id, manager_id)
-VALUES("Gorge", "Michel", 1, 2),
-         ("Nicolas", "Hummings", 2, null),
-        ("Justica", "Fernandez", 3, 4),
+VALUES("George", "Michel", 1, null),
+        ("Nicolas", "Hummings", 2, null),
+        ("Justica", "Fernandez", 3, null),
         ("Mark", "Twain", 4, null),
-        ("Nicole", "Smith", 5, 6),
+        ("Nicole", "Smith", 5, null),
         ("Allan", "Ancira", 6, null),
-        ("Sophia", "Espinosa", 7, 8),
-         ("Bob", "Rob", 8, null);
+        ("Sophia", "Espinosa", 7, null),
+        ("Bob", "Rob", 8, null);
